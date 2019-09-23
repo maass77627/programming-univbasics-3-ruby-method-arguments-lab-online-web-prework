8c494daa0a685b815e25a2a0e3d1635e2e78f041
kelly r maass <kmaass77627@gmail.com>
2019-09-23 01:36:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,14 @@ def introduction(name)
 end 
 instroduction 
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
+introduction_with_language
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
 
 
 
